b1f53f8c4735bcf0d5a56f21ca66667127085bd4
Björn Ahl <bnal@ba78.me>
2020-05-07 14:11:13 +0200

    feature: freecodecamp - Api APIs and Microservices Projects - File Meta Data

---
+++ FileMetaData/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+
+let upload = multer({ dest: 'uploads/' })
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { size, filename, mimetype } = req.file;
+
+  res.json({
+    name: filename,
+    type: mimetype,
+    size: size
+  })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
